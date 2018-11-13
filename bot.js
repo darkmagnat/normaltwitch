6424652434b5feb90c9c9a9e685f4f7bb4541a9b
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-13 18:45:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,7 @@
-const Discord = require('discord.js');
-
-const Util = require('discord.js');
-
-const getYoutubeID = require('get-youtube-id');
-
-const fetchVideoInfo = require('youtube-info');
-
-const YouTube = require('simple-youtube-api');
-
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-
-const queue = new Map();
-
-const ytdl = require('ytdl-core');
-
-const fs = require('fs');
-
-const gif = require("gif-search");
-
-const client = new Discord.Client({disableEveryone: true});
+const Discord = require('discord.js')
+const client = new Discord.Client();
+client.on('ready', function(){
+ console.log(`Logged in as ${client.user.tag}!`);
 
 const prefix = "#";
 ///////////////////////////
