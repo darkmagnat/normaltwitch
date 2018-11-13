ae3667bede4f1b4a02de50bb3deba8ea3002b7fc
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-13 19:14:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,14 @@ const Discord = require('discord.js');
 
 const Util = require('discord.js');
 
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
 const queue = new Map();
 
 const ytdl = require('ytdl-core');
@@ -11,8 +19,30 @@ const fs = require('fs');
 const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
-///////////////////////////
-///////////////////////////
+
+const prefix = "#";
+/////////////////////////
+////////////////////////
+
+client.on('message', async msg =>{
+	if (msg.author.bot) return undefined;
+    if (!msg.content.startsWith(prefix)) return undefined;
+    
+    let args = msg.content.split(' ');
+
+	let command = msg.content.toLowerCase().split(" ")[0];
+	command = command.slice(prefix.length)
+
+    if(command === `ping`) {
+    let embed = new Discord.RichEmbed()
+    .setColor(3447003)
+    .setTitle("Pong!!")
+    .setDescription(`${client.ping} ms,`)
+    .setFooter(`Requested by | ${msg.author.tag}`);
+    msg.delete().catch(O_o=>{})
+    msg.channel.send(embed);
+    }
+});
 const adminprefix = "#";
 const devs = ['400250690284421120'];
 client.on('message', message => {
@@ -45,5 +75,8 @@ if (message.content.startsWith(adminprefix + 'st')) {
       message.channel.sendMessage(`**✅   ${argresult}**`)
  }
 });
+
+
+
 
 client.login(process.env.BOT_TOKEN);
